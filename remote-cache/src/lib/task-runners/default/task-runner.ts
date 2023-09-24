9dcb2f11c974aa14119097b026a047d2224a2172
Ilya <iluhamishkin@gmail.com>
2023-09-24 22:50:55 +0200

    Fix typing for default task runner

--- remote-cache/src/lib/task-runners/default/task-runner.ts
+++ remote-cache/src/lib/task-runners/default/task-runner.ts
@@ -1,11 +1,12 @@
 import { defaultTasksRunner } from '@nrwl/devkit';
 import { Logger } from '../../util/Logger';
+import { DefaultTaskRunnerOutput } from '../../util/DefaultTaskRunnerOutput';
 
 export function taskRunner(
 	tasks: Parameters<typeof defaultTasksRunner>[0],
 	options: Parameters<typeof defaultTasksRunner>[1],
 	context: Parameters<typeof defaultTasksRunner>[2]
-): string {
+): DefaultTaskRunnerOutput {
 	const verboseLogging: boolean = process.env['NX_CACHE_VERBOSE_LOGGING'] === 'true';
 	const logger: Logger = new Logger('DEFAULT', verboseLogging);
 
